769c8bf9b41e0f48ebd8a5386922e53c49c957b9
Kostya <skulkinka@gmail.com>
2011-04-13 11:51:08 +0400

    Correted project name and namespace

--- DoWorkEmbeddedTest/Properties/AssemblyInfo.cs
+++ DoWorkEmbeddedTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BackgroundOperationEmbedded")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("DoWorkEmbeddedTest")]
+[assembly: AssemblyDescription("Background operation handling with embedded controls")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("BackgroundOperationEmbedded")]
+[assembly: AssemblyCompany("Skatech research Lab")]
+[assembly: AssemblyProduct("DoWorkEmbeddedTest")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
